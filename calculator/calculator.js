2fbb7ac1d516048810082b7becdee92d653677a5
twigpi <55249972+twigpi@users.noreply.github.com>
2022-04-04 02:59:51 -0400

    Assignment complete
    Fulfill all requirements

--- calculator/calculator.js
+++ calculator/calculator.js
@@ -1,35 +1,132 @@
-class Calculator {
+class Calculator
+{
     firstNumber;
     secondNumber;
     operator;
     value;
 
-    constructor(firstNumber, secondNumber, operator) {
+    /**
+     * @param {Number} firstNumber
+     * @param {Number} secondNumber
+     * @param {MATH_OPERATOR} operator
+     */
+    constructor(firstNumber, secondNumber, operator)
+    {
         this.firstNumber = firstNumber;
         this.secondNumber = secondNumber;
         this.operator = operator;
         this.value = 0;
     }
 
-    // determine the current operation's label
-    getAction() {}
+    /**
+     * determine the current operation's label
+     * a bit unsure as to why this is here
+     */
+    getAction()
+    {
+        return this.operator;
+    }
 
-    // performa a calculation based on the currently selected operation
-    operate() {}
-
-    attemptedDivideByZero = false;
+    // perform a calculation based on the currently selected operation
+    operate()
+    {
+        performCalculation();
+    }
 
     // perform addition operation
-    add() {}
+    add()
+    {
+        this.value = this.firstNumber + this.secondNumber;
+    }
 
     // perform subtraction operation
-    subtract() {}
+    subtract()
+    {
+        this.value = this.firstNumber - this.secondNumber;
+    }
 
     // perform multiplication operation
-    multiply() {}
+    multiply()
+    {
+        this.value = this.firstNumber * this.secondNumber;
+    }
 
     // perform divide operation
-    divide() {}
+    divide()
+    {
+        if (this.secondNumber !== 0)
+            this.value = this.firstNumber / this.secondNumber;
+        else
+            this.value = this.firstNumber + 'ⱺ';
+    }
+
+        /**
+         * @returns Boolean
+         */
+        isDivideByZeroScenario()
+        {
+            let isScenario = ( (this.operator === "Divide")
+                            && (this.secondNumber === 0) );
+
+            return isScenario;
+        }
+
+    performCalculation()
+    {
+        if (this.operator === "Add")
+            this.add();
+        else if (this.operator === "Subtract")
+            this.subtract();
+        else if (this.operator === "Multiply")
+            this.multiply();
+        else if (this.operator === "Divide")
+            this.divide();
+        else
+            alert("Operator: " + this.operator + " not set.");
+    }
+
+    /**
+     * @param {Number} firstNumber
+     */
+    setFirstNumber(firstNumber)
+    {
+        let value = Number(firstNumber);
+        if ( value !== NaN )
+            this.firstNumber = value;
+        else
+            this.firstNumber = 0;
+    }
+
+    /**
+     * @param {Number} secondNumber
+     */
+    setSecondNumber(secondNumber)
+    {
+        let value = Number(secondNumber)
+        if ( value !== NaN )
+            this.secondNumber = value;
+        else
+            this.secondNumber = 0;
+    }
+
+    /**
+     * @param {MATH_OPERATOR} operator
+     */
+    setOperator(operator)
+    {
+        if ( MATH_OPERATOR.hasOwnProperty(operator) )
+            this.operator = operator;
+        else
+            this.operator = "Add";
+    }
+
+    /**
+     * @returns Number
+     */
+    getValue()
+    {
+        return this.value;
+    }
 }
 
 //DO NOT TOUCH THIS LINE OF CODE//
--- calculator/controller.js
+++ calculator/controller.js
@@ -1,20 +1,337 @@
+const CALCULATOR_STATUS =
+{
+    Initial :       ["Initial", "Calculator state before anything happens."],
+    Ready :         ["Ready", "Awaiting user input."],
+    Inputting :     ["Inputting", "Receiving input."],
+    Checking :      ["Checking", "Checking for valid input."],
+    Sanitizing :    ["Cleaning", "Sanitizing nonnumerical data with 70% isopropyl alchohol."],
+    Calculating :   ["Calculated", "Numbers working according to plan."],
+    Results :       ["Results", "The ${resultName} ${prepo} ${firstTerm} ${conjunct} ${secondTerm} is ${value}."],
+    Error :         ["Error", "An error has occurred. Hope the developer fixes it soon."],
+    Gge :           ["Gge", "ollǝɥ"],
+    Unknown :       ["Unknown State", "We're not in Kansas anymore."],
+    DivideByZero :  ["Divide by Zero", "Warning: Dividing by zero is not allowed until the ⱺ numberline is discovered."]
+}
+
+const CALCULATOR_MODE =
+{
+    Add :       "Adding",
+    Subtract :  "Subtracting",
+    Multiply :  "Multiplying" ,
+    Divide :    "Dividing"
+}
+
+const MATH_OPERATOR =
+{
+    "Add" :         ["+", "Augend", "Addend", "Sum", "of", "and"],
+    "Subtract" :    ["-", "Minuend", "Subtrahend", "Difference", "between", "and"],
+    "Multiply" :    ["*", "Multiplicand", "Multiplier", "Product", "of", "times"],
+    "Divide" :      ["/", "Dividend", "Divisor", "Quotient", "of", "divided by"],
+    "Nully" :       ["E", "Nullend", "Nullifier", "Nullent", "of", "nullified by"]
+}
+
+let abacus;
+
+window.onload = CreateCalculator();
+
 // setup variables and get a calculator instance
-function CreateCalculator() {}
+function CreateCalculator()
+{
+    this.abacus = new Calculator(0, 0, "Add");
+
+    setCalculatorNumbersFromInput();
+    setCalculatorOperatorFromInput();
+    clearResult();
+
+
+    addEventListeners();
+
+    setStatus("Ready");
+}
+
+    function setCalculatorNumbersFromInput()
+    {
+        let inputElement;
+
+        inputElement = document.getElementById("inputNumberOne");
+        this.abacus.setFirstNumber(inputElement.value);
+
+        inputElement = document.getElementById("inputNumberTwo");
+        this.abacus.setSecondNumber(inputElement.value);
+    }
+
+    function setCalculatorOperatorFromInput()
+    {
+        let operator = getOperator();
+
+        this.abacus.operator = operator;
+        setMode(operator);
+    }
+
 
 // perform a calculation when the operator button is clicked
-function calculate() {}
+function calculate()
+{
+    setStatus("Calculating");
+    sanitizeTermInputs();
+    setCalculatorNumbersFromInput();
+    setCalculatorOperatorFromInput();
+
+    let result;
+    let message;
+    if (this.abacus.isDivideByZeroScenario() === false)
+    {
+        this.abacus.performCalculation();
+        result = this.abacus.getValue();
+        setStatus("Results");
+    }
+    else
+    {
+        result = CALCULATOR_STATUS.DivideByZero[0];
+        setStatus("DivideByZero");
+    }
+
+    updateResultText(result);
+}
 
 /**
  * set the text in the result section of the UI
  * @param {*} value
  */
-function updateResultText(value) {}
+function updateResultText(value)
+{
+    let textField = document.getElementById("calculation-result");
+    textField.innerText = value;
+}
+
+function displayMessage(messageText)
+{
+    let textField = document.getElementById("calculator-message");
+    textField.innerText = messageText;
+}
 
 // should clear input text values and focus the first number input
-function clearValues() {}
+function clearValues()
+{
+    setStatus("Initial");
+
+    clearResult();
+
+    let inputField;
+
+    inputField = document.getElementById("add");
+    inputField.click();
+
+    inputField = document.getElementById("inputNumberTwo");
+    inputField.value = "";
+
+    inputField = document.getElementById("inputNumberOne");
+    inputField.value = "";
+    inputField.focus();
+
+    setStatus("Ready");
+}
+
+function clearResult()
+{
+    let textField = document.getElementById("calculation-result");
+    textField.innerText = "0";
+}
 
 /**
  * get the selected operator from the UI
  * @returns Operators
  */
-function getOperator() {}
+function getOperator()
+{
+    let operator;
+
+    let inputArray = document.getElementsByName("operation-panel");
+
+    let index = inputArray.length;
+    let exitLoop = false;
+    while (exitLoop === false)
+    {
+        index--;
+
+        exitLoop = ( inputArray[index].checked
+                  || index === 0 )
+    }
+
+    if (0 <= index && index <= inputArray.length)
+        operator = inputArray[index].value;
+    else
+        alert("index out of bounds for inputArray in getOperator()");
+
+    if (MATH_OPERATOR.hasOwnProperty(operator) === false)
+        operator = "Nully";
+
+    return operator;
+}
+
+
+/**
+ * Calculator status indicates what the calculator is doing or expecting.
+ * @param {CALCULATOR_STATUS} status
+ */
+function setStatus(status)
+{
+    if ( CALCULATOR_STATUS.hasOwnProperty(status) === false )
+        status = "Unknown";
+
+    let textField = document.getElementById("calculator-status");
+
+    textField.innerText = CALCULATOR_STATUS[status][0];
+    if (status !== "Results")
+        displayMessage(CALCULATOR_STATUS[status][1]);
+    else
+        DisplayResultsMessage();
+}
+
+    function DisplayResultsMessage()
+    {
+        let template = CALCULATOR_STATUS.Results[1];
+        let operator = getOperator();
+        const termsMap =
+        {
+            "resultName" : MATH_OPERATOR[operator][3],
+            "prepo" : MATH_OPERATOR[operator][4],
+            "firstTerm" : this.abacus.firstNumber,
+            "conjunct" : MATH_OPERATOR[operator][5],
+            "secondTerm" : this.abacus.secondNumber,
+            "value" : this.abacus.getValue()
+        } ;
+        let message = interpolate(template, termsMap);
+        displayMessage(message);
+    }
+
+    /**
+     * Helper function to perform interpolation on stored template literals.
+     * termsMap is an object containing the keywords to search for in template
+     *  and their replacement text.
+     * @param {String} template
+     * @param {Object} termsMap
+     * @returns String
+     */
+    function interpolate(template, termsMap)
+    {
+        for (term in termsMap)
+        {
+            template = template.replace( "${" + term + "}", String(termsMap[term]).toLowerCase() );
+        }
+
+        return template;
+    }
+
+/**
+ * Calculator mode is what operation will be used.
+ * @param {MATH_OPERATOR} operator
+ */
+function setMode(operator)
+{
+    if (MATH_OPERATOR.hasOwnProperty(operator) === false)
+        operator = "Nully";
+
+    let textField;
+
+    textField = document.getElementById("calculator-operation");
+    textField.innerText = MATH_OPERATOR[operator][0];
+
+    let labelText;
+
+    labelText = document.getElementById("div-term-one-label")
+    labelText.innerText = MATH_OPERATOR[operator][1];
+
+    labelText = document.getElementById("div-term-two-label")
+    labelText.innerText = MATH_OPERATOR[operator][2];
+
+    labelText = document.getElementById("div-calculation-result-label")
+    labelText.innerText = MATH_OPERATOR[operator][3];
+}
+
+/**
+ * At least one browser (~cough, rhymes with "higher socks") still allows non-number
+ *  input to appear in a number input box. This function displays the correct values.
+ */
+function sanitizeTermInputs()
+{
+    setStatus("Sanitizing");
+    let textToSanitize;
+
+    let inputFieldArray;
+
+    inputFieldArray = document.getElementsByClassName("input-term");
+    for (let index = 0; index < inputFieldArray.length; index++)
+    {
+        textToSanitize = inputFieldArray[index].value;
+        inputFieldArray[index].value = Number(textToSanitize);
+    }
+}
+
+    /**
+     * No longer needed, but left as a relic containing my own regex
+     * @param {String} numberText
+     * @returns Boolean
+     */
+    function isValidNumber(numberAsText)
+    {
+        setStatus("Checking");
+        const pattern = /^[+-]?\d*(?:[.]\d+)?(?:e[+-]\d+)?$/
+        answer = pattern.test(numberAsText);
+        answer &= ( Number(numberAsText) !== NaN );
+
+        return answer;
+    }
+
+
+function addEventListeners()
+{
+    let inputFieldArray = document.getElementsByClassName("input-term");
+    for (let index = 0; index < inputFieldArray.length; index++)
+    {
+        inputFieldArray[index].addEventListener("focus", onInputFocus);
+        inputFieldArray[index].addEventListener( "keypress", function(event) {event.which === 13 ? onInputPressEnter() : 0} );
+    }
+
+    let inputButton;
+
+    inputButton = document.getElementById("button-perform-calculation");
+    inputButton.addEventListener("click", onCalculateButtonClick);
+
+    inputButton = document.getElementById("button-reset");
+    inputButton.addEventListener("click", onResetButtonClick);
+
+    let inputArray = document.getElementsByName("operation-panel");
+    for (let index = 0; index < inputArray.length; index++)
+    {
+        inputArray[index].addEventListener("click", onOperationPanelClick);
+    }
+}
+
+    function onInputFocus()
+    {
+        setStatus("Inputting");
+        clearResult();
+    }
+
+    function onResetButtonClick()
+    {
+        clearValues();
+    }
+
+    function onOperationPanelClick()
+    {
+        clearResult();
+        setStatus("Inputting");
+        setCalculatorOperatorFromInput();
+    }
+
+    function onCalculateButtonClick()
+    {
+        calculate();
+    }
+
+    function onInputPressEnter()
+    {
+        calculate();
+    }
